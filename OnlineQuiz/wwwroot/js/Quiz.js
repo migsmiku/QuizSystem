6779b3c468844b44059d7a9f94cac5b54a8235c7
Migs <migsmiku@gmail.com>
2023-01-15 13:44:19 -0800

    Finish QuizResult

--- OnlineQuiz/wwwroot/js/Quiz.js
+++ OnlineQuiz/wwwroot/js/Quiz.js
@@ -1,10 +1,14 @@
 ﻿var countDownDate; // 15 minutes in milliseconds
 
 if (localStorage.getItem("countDownDate")) {
+    var current = new Date().getTime();
+    var expire = sessionStorage.getItem("expire");
     var retrievedValue = parseInt(localStorage.getItem("countDownDate"));
-    countDownDate = !isNaN(retrievedValue) ? retrievedValue : new Date().getTime() + 900000;
+    countDownDate = !isNaN(retrievedValue) && current > expire? retrievedValue : new Date().getTime() + 900000;
 } else {
-    countDownDate = new Date().getTime() + 900000;
+    var countTime = new Date().getTime() + 900000;
+    sessionStorage.setItem("expire", countTime);
+    countDownDate = countTime;
 }
 localStorage.setItem("countDownDate", countDownDate);
 
@@ -20,11 +24,11 @@ var x = setInterval(function () {
     if (distance < 0) {
         clearInterval(x);
         alert("Time's up!");
-        resetTimer();
+        resetLocalStorage();
         document.getElementById("quizForm").submit();
     }
 }, 1000);
 
 function resetTimer() {
-    localStorage.removeItem("countDownDate");
+    localStorage.clear();
 }
