8b3b266f56e1843d13d15633044c85a96ba137b6
Migs <migsmiku@gmail.com>
2023-01-16 14:20:40 -0800

    Modify quiz result display

--- OnlineQuiz/DAL/QuizDAL.cs
+++ OnlineQuiz/DAL/QuizDAL.cs
@@ -245,7 +245,7 @@
                     quizSubmissions.QuizId = (int)reader["QuizId"];
                     quizSubmissions.UserId = (int)reader["UserId"];
                     quizSubmissions.StartTime = (DateTime)reader["StartTime"];
-                    quizSubmissions.EndTime = reader["EndTime"] is null?(DateTime)reader["EndTime"]:null;
+                    quizSubmissions.EndTime = reader["EndTime"] is not null?(DateTime)reader["EndTime"]:null;
                     quizSubmissions.Score = (int)reader["Score"];
                     quizSubmissions.CreatedDate = (DateTime)reader["CreatedDate"];                        
                 }
